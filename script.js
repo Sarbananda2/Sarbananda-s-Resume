e0a5e5003a19edd82678dfc386f7469925442d79
Sarbananda Taw <sarbanandataw2@gmail.com>
2023-10-13 16:57:06 +0530

    Updated Site with New Version
    * Added new index.html file
    * Added new style.css file
    * Added new script.js file

---
+++ script.js
@@ -0,0 +1,34 @@
+// JavaScript function to display a personalized greeting with two parts (Hey there and Good Morning/Afternoon/Evening)
+function displayGreeting() {
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+
+    let timeOfDay;
+    if (currentHour < 12) {
+        timeOfDay = 'Morning';
+    } else if (currentHour < 18) {
+        timeOfDay = 'Afternoon';
+    } else {
+        timeOfDay = 'Evening';
+    }
+
+    const partA = 'Hey there, Traveler';
+    const partB = `Good ${timeOfDay}`;
+    const partAElement = document.getElementById('partA');
+    const partBElement = document.getElementById('partB');
+    partAElement.textContent = partA;
+    partBElement.textContent = partB;
+}
+
+// JavaScript function to redirect to the final resume (PDF)
+function showFinalResume() {
+    window.open('resume.pdf', '_blank');
+}
+
+// JavaScript function to redirect to the comment page (External Link)
+function commentOnResume() {
+    window.open('https://app.resumelab.com/feedback/session/c1d6d21c-f2c5-4afd-b2b3-7d5638f4b120', '_blank');
+}
+
+// Call the function to display a personalized greeting with two parts
+displayGreeting();
